04fafed86101ee37462576b2d6e364f55aa5011b
andrepatta <9854773+andrepatta@users.noreply.github.com>
2026-01-12 02:15:52 -0300

    add Nostr NIP-05 metadata

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/.well-known/nostr.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
